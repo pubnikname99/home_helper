54672eb73fac3af38a184fa91cfbe36d564025b6
pubnikname99 <work-nik@fedora-lt1>
2024-08-19 12:49:20 +0300

    the app can now extract some data from text files and has a notes page added

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
+year = date.today().year
 
+with (open("data/backgrounds.txt") as backgrounds,
+      open("data/watchies.txt") as watchies,
+      open("data/soundies.txt") as soundies,
+      open('data/notes.txt') as notes):
+    backgrounds = backgrounds.read().splitlines()
+    watchies = watchies.read().splitlines()
+    soundies = soundies.read().splitlines()
+    extracted_notes = notes.read().splitlines()
+    #users = [line.removeprefix("User: ") for line in raw_input if line.startswith("User: ")]
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', year=year, backgrounds=backgrounds, watchies=watchies, soundies=soundies)
 
+
+@app.route("/notes")
+def notes():
+    return render_template('notes.html', year=year, extracted_notes=extracted_notes)
+
+
+# # Variable routing
+# @app.route("/<username>")
+# def userspace(username):
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=23232, debug=True)
